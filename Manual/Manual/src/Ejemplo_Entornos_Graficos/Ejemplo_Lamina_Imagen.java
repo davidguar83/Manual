e9cb4c24cb6ebaa6cd98b98c371d1ddbffc35e57
david <david@DESKTOP-AUJP76H>
2020-07-27 21:01:38 +0200

    Merge origin/master
    Conflicts:
    	Manual/Manual/src/Ejemplo_Entornos_Graficos/Ejemplo_Lamina_Texto.java

---
+++ Manual/Manual/src/Ejemplo_Entornos_Graficos/Ejemplo_Lamina_Imagen.java
@@ -0,0 +1,83 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Ejemplo_Entornos_Graficos;
+
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+/**
+ *
+ * @author david
+ */
+public class Ejemplo_Lamina_Imagen {
+    
+    public static void main(String[]args){
+        
+        MarcoImagen marcoimagen = new MarcoImagen();
+        
+        
+        marcoimagen.setVisible(true);
+        
+        marcoimagen.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        
+        
+    }
+    
+    
+    
+    
+}
+
+
+
+class MarcoImagen extends JFrame{
+
+    public MarcoImagen() {
+        
+        
+        setTitle("Lamina con imagen");
+        
+        setBounds(500, 500, 500, 500);
+        
+        
+        
+       LaminaImagen milaminaimagen = new LaminaImagen();
+       
+       add(milaminaimagen);
+        
+        
+        
+        
+    }
+    
+    
+    
+    
+}
+
+
+
+class LaminaImagen extends JPanel{
+    
+    @Override
+    protected void paintComponent(Graphics g) {
+        
+        
+        super.paintComponent(g); 
+    
+        Graphics2D g2 = (Graphics2D)g;
+    
+    
+    
+    }
+    
+    
+
+    
+    
+}
