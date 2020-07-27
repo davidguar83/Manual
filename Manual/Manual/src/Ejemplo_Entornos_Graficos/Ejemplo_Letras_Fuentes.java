a256ec31cd54197277c62ae24e2e819cc5964c26
david <david@DESKTOP-AUJP76H>
2020-07-27 13:14:10 +0200

    Merge origin/master
    Conflicts:
    	Manual/Manual/src/Ejemplo_Entornos_Graficos/Ejemplo_Lamina_Texto.java

---
+++ Manual/Manual/src/Ejemplo_Entornos_Graficos/Ejemplo_Letras_Fuentes.java
@@ -0,0 +1,64 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Ejemplo_Entornos_Graficos;
+
+import java.awt.GraphicsEnvironment;
+
+import javax.swing.*;
+
+/**
+ *
+ * @author david
+ */
+public class Ejemplo_Letras_Fuentes {
+    
+    
+    public static void main(String[]args){
+        
+        String fuentes= JOptionPane.showInputDialog("Introduce fuente letras");
+        
+        boolean comprobacionletra=false;
+        
+        
+        String [] letrasfuentes= GraphicsEnvironment.getLocalGraphicsEnvironment().getAvailableFontFamilyNames();
+        
+        
+        
+        for(String nombreLetras: letrasfuentes){
+            
+            
+            if(nombreLetras.equals(fuentes)){
+                
+                comprobacionletra=true;
+                
+            }
+            
+          //  System.out.println(nombreLetras); 
+            
+        }
+        
+        
+        if(comprobacionletra){
+            
+            
+            System.out.println("Fuente de letra instalada");
+            
+            
+        }else{
+            
+            
+            System.out.println("Fuente de lerta no instalada");
+            
+        }
+        
+        
+        
+        
+        
+        
+    }
+    
+}
