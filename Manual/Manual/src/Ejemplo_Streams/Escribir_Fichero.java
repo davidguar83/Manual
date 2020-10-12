37992375fe5302356ecbcdbdd6625dd6091285c7
david <david@DESKTOP-AUJP76H>
2020-10-12 15:10:20 +0200

    Merge origin/master
    Conflicts:
    	Manual/Manual/src/Ejemplo_Entornos_Graficos/Ejemplo_Lamina_Texto.java

---
+++ Manual/Manual/src/Ejemplo_Streams/Escribir_Fichero.java
@@ -0,0 +1,69 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Ejemplo_Streams;
+
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author david
+ */
+public class Escribir_Fichero {
+    
+    
+    
+    public static void main(String[] args) {
+        
+        
+        Escribiendo a = new Escribiendo();
+        
+        
+        a.escribir();
+        
+        
+        
+        
+    }
+    
+}
+
+
+
+class Escribiendo{
+    
+    public void escribir(){
+        
+        String frase = " esto es una prueba de escritura";
+        
+        
+        try {
+            FileWriter escritura = new FileWriter("C:/Users/david/Desktop/prueba clase file/texto_escritura.txt");
+            
+            for( int i=0;i<frase.length();i++){
+                
+                escritura.write(frase.charAt(i));
+                
+                
+                
+                
+            }
+            escritura.close();
+            
+        } catch (IOException e) {
+            System.out.println("Archibo o escontrado");
+        }
+        
+        
+        
+        
+    }
+    
+    
+    
+}
